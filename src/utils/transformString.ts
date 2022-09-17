c8837bf75bdaae3a0ed2660fde1ba5ddddc0b410
Safwan Shaheer <devorein00@gmail.com>
2022-09-17 15:09:37 +0600

    Remove react-query and zod dependencies from FormikForm (#6)

--- src/utils/transformString.ts
+++ src/utils/transformString.ts
@@ -1,15 +1,30 @@
 export function transformString(
   inputStr: string,
-  transformation?: 'capitalize' | 'split_capitalize'
+  transformation?: 'capitalize' | 'split_capitalize' | "pascal"
 ) {
   transformation = transformation ?? 'capitalize';
 
   if (transformation === 'capitalize') {
     return inputStr[0].toUpperCase() + inputStr.slice(1);
-  } else {
+  } else if (transformation === "split_capitalize") {
     return inputStr
       .split('_')
       .map((chunk) => chunk[0].toUpperCase() + chunk.slice(1))
       .join(' ');
+  } else if (transformation === "pascal") {
+    let converted = '';
+
+    for (const char of inputStr) {
+      const charCode = char.charCodeAt(0);
+      if (charCode >= 65 && charCode <= 90) {
+        converted += ` ${char.toLowerCase()}`;
+      } else {
+        converted += char;
+      }
+    }
+
+    return converted.charAt(0).toUpperCase() + converted.slice(1);
   }
+
+  return inputStr;
 }
