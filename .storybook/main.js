c763f4608130ca1b2eb184a493a041ca43d807a1
Safwan Shaheer <devorein00@gmail.com>
2022-09-17 15:20:07 +0600

    Added storybook deps and configs

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../stories/**/*.stories.tsx'],
+  staticDirs: ['../public'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  typescript: {
+    reactDocgen: 'none'
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, '../')
+    ];
+
+    return config;
+  }
+};
