a1ad2d9e46cb3be8cefffb29ef7b135b27463fd4
Safwan Shaheer <devorein00@gmail.com>
2022-09-17 15:37:27 +0600

    Added Select input story

---
+++ stories/SelectInput.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, Story } from '@storybook/react';
+import { Formik } from 'formik';
+
+import { SelectInput, SelectInputProps } from '../src/components';
+
+export default {
+  title: 'Components/Form/Select Input',
+  component: SelectInput
+} as Meta;
+
+const Template: Story<SelectInputProps> = (props) => {
+  return (
+    <Formik
+      initialValues={{
+        role: 'user'
+      }}
+      onSubmit={() => {}}
+      validateOnMount
+    >
+      <SelectInput {...props} />
+    </Formik>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  name: 'role',
+  label: 'Role',
+  helperText: 'Select your role',
+  values: [
+    {
+      label: 'Admin',
+      value: 'admin'
+    },
+    {
+      label: 'User',
+      value: 'user'
+    }
+  ]
+};
