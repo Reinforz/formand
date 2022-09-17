ad3b1bf04350e27a0bfa1079e37778b19ef1f0ec
Safwan Shaheer <devorein00@gmail.com>
2022-09-17 14:50:03 +0600

    Added formik form component

---
+++ src/components/FormikForm.tsx
@@ -0,0 +1,137 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Stack,
+  SxProps,
+  Typography
+} from '@mui/material';
+import { Form, Formik, FormikConfig } from 'formik';
+import { ReactNode } from 'react';
+import { FormConstants, FormInputs } from '../types';
+import { FormikFormInputs } from './FormikFormInput';
+import LoadingButton from './LoadingButton';
+
+export interface FormikFormProps<
+  Payload extends Record<string, any>
+> {
+  formPreSubmit?: ReactNode;
+  formFooter?: ReactNode;
+  formConstants: FormConstants<Payload> & {
+    payloadFactory: () => Payload;
+    validationSchema: any;
+  };
+  formInputs: FormInputs<Payload>;
+  onSubmit: FormikConfig<Payload>["onSubmit"];
+  formBodySx?: SxProps;
+  formSx?: SxProps;
+  isLoading?: boolean
+}
+
+export function FormikForm<Payload extends Record<string, any>>(
+  props: FormikFormProps<Payload>
+) {
+  const {
+    isLoading = false,
+    formFooter,
+    formConstants: {
+      payloadFactory,
+      validationSchema,
+      formHeaderText,
+      onLoadButtonText,
+      submitButtonText,
+      label,
+      placeholder,
+      helperText,
+      optionalFields = [],
+      formHeaderHelperText
+    },
+    formBodySx = {},
+    formInputs,
+    formPreSubmit,
+    formSx = {},
+    onSubmit
+  } = props;
+
+  return (
+    <Formik
+      validateOnBlur={false}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      validateOnMount
+      validateOnChange={true}
+      initialValues={payloadFactory()}
+    >
+      {({ isSubmitting, isValid }) => (
+        <Box sx={formSx}>
+          <Form
+            style={{
+              height: 'fit-content',
+              width: '100%'
+            }}
+          >
+            <Stack
+              gap={1}
+              my={2}
+              sx={{
+                display: { xs: 'flex', sm: 'initial' },
+                flexDirection: { xs: 'column', sm: 'initial' },
+                alignItems: { xs: 'center', sm: 'initial' },
+                my: { md: 3 }
+              }}
+            >
+              <Stack
+                flexDirection='row'
+                alignItems='center'
+                sx={{
+                  justifyContent: { sm: 'center', xs: 'normal' },
+                  display: 'initial'
+                }}
+              >
+                <Typography variant='h5'>{formHeaderText}</Typography>
+              </Stack>
+              {formHeaderHelperText && (
+                <Typography
+                  variant='subtitle1'
+                  sx={{
+                    opacity: 0.75,
+                    display: { xs: 'none', sm: 'initial' }
+                  }}
+                >
+                  {formHeaderHelperText}
+                </Typography>
+              )}
+              <Divider
+                sx={{
+                  my: 2
+                }}
+              />
+            </Stack>
+            <FormikFormInputs<Payload>
+              formInputs={formInputs}
+              label={label}
+              placeholder={placeholder as any}
+              helperText={helperText}
+              optionalFields={optionalFields}
+              isDisabled={isSubmitting}
+              sx={formBodySx}
+              mb={2}
+              gap={2}
+            />
+            {formPreSubmit}
+            <Stack flexDirection='row' justifyContent='space-between' mb={3}>
+              {isLoading ? (
+                <LoadingButton disabled>{onLoadButtonText}</LoadingButton>
+              ) : (
+                <Button type='submit' disabled={isSubmitting || !isValid}>
+                  {submitButtonText}
+                </Button>
+              )}
+            </Stack>
+            {formFooter}
+          </Form>
+        </Box>
+      )}
+    </Formik>
+  );
+}
---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,28 @@
+import { Button, ButtonProps, CircularProgress } from '@mui/material';
+import { ReactNode } from 'react';
+
+interface LoadingButtonProps extends ButtonProps {
+  children: ReactNode
+}
+
+export default function LoadingButton({ children, ...props }: LoadingButtonProps) {
+  return (
+    <Button
+      variant='contained'
+      sx={{
+        maxWidth: 250,
+        alignSelf: 'center'
+      }}
+      {...props}
+    >
+      <CircularProgress
+        size={20}
+        sx={{
+          color: 'white',
+          mr: 1
+        }}
+      />
+      {children}
+    </Button>
+  );
+}
