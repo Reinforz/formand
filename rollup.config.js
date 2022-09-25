93a1e7cddd17317f503655b2a0173a0518ac1516
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2022-09-25 14:14:40 +0600

    fix: unset side effects and use sigle quote

--- rollup.config.js
+++ rollup.config.js
@@ -6,26 +6,29 @@ import packageJson from './package.json'
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
-        file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
-        dir: "dist",
+        dir: 'dist',
         preserveModules: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
   },
   {
-    input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
 ];
